e9d553b1f6873b07632ac60e749dc93f445d3031
teamrun <chenllos@163.com>
2014-09-13 23:45:12 +0800

    完善开场和落幕

--- video/js/app.js
+++ video/js/app.js
@@ -10,7 +10,24 @@ React.renderComponent(<ChatList scrollEle="#msg-module"/>, $('#msg-module .list-
 
 
 $('.simulate-video audio').on('play', function(){
-    // setTimeout(function(){
-        MovieCtrl.start();
-    // }, 1500);
+    MovieCtrl.start();
+    setTimeout(function(){
+        $('#msg-module').removeClass('hidden');
+    }, 2400);
+
+    setTimeout(function(){
+        $('.mask.curtain').removeClass('hidden');
+        setTimeout(function(){
+            $('.movie-title').removeClass('hidden');
+        }, 2000);
+    // before wardo: mark!!!
+    },55350);
+
+    // 隐藏掉控制条
+    $(this).css('opacity', 0);
+    var self = this;
+    setTimeout(function(){
+        $(self).removeAttr('controls');
+    }, 1000);
+    
 });
